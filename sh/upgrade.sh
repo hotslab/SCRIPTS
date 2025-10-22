6bceca96cdcd20784dfd3e94a6b46fbf8ac69804
Joseph Nyahuye <joseph.nyahuye@gmail.com>
2025-10-22 23:09:14 +0200

    refactor

--- sh/upgrade.sh
+++ sh/upgrade.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo " " && \
-    echo "Upgrading apt packages..." && \
-    echo " " && \
-    sudo apt upgrade -y && \ 
-    echo " " && \
-    echo "Auto removing apt packages..." && \
-    echo " " && \
-    sudo apt autoremove -y && \
-    echo " " && \
-    echo "Auto cleaning  apt packages..." && \
-    echo " " && \
-    sudo apt autoclean -y && \
-    echo " " && \
-    echo "Upgrading flatpack packages..." && \
-    echo " " && \
-    flatpak update -y && \
-    echo " " && \
-    echo "Removing unused flatpak packages..." && \
-    echo " " && \
-    flatpak uninstall --unused -y
+echo " " 
+echo "Upgrading apt packages..." 
+echo " "
+sudo apt upgrade -y
+echo " "
+echo "Auto removing apt packages..." 
+echo " " 
+sudo apt autoremove -y 
+echo " " 
+echo "Auto cleaning  apt packages..." 
+echo " " 
+sudo apt autoclean -y
+echo " "
+echo "Upgrading flatpack packages..."
+echo " "
+flatpak update -y
+echo " "
+echo "Removing unused flatpak packages..."
+echo " "
+flatpak uninstall --unused -y
